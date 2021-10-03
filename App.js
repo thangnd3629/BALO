1357ac77962555d346be51a48e6f0cc88b4f8153
Nguyen Duc Thang <hsgsdominate@gmail.com>
2021-10-03 16:59:24 +0700

    Init project

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View, Button } from "react-native"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container]}>
+      <View style={{ flex: 3, backgroundColor: "green" }} />
+      <View
+        style={{
+          position: "absolute",
+          left: 0,
+          top: 0,
+          flex: 1,
+          backgroundColor: "green",
+        }}
+      >
+        <Button title={"hello"}></Button>
+      </View>
     </View>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 0,
   },
-});
+})
