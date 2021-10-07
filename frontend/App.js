1bd9a46dc3d23a910c60066423321d4ca4e5dd13
Nguyen Duc Thang <hsgsdominate@gmail.com>
2021-10-07 17:06:14 +0700

    Sign in screen

--- frontend/App.js
+++ frontend/App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View, Button } from "react-native"
 import HeaderWithSearchBar from "./components/HeaderWithSearchBar"
 import Login from "./screen/Login"
 import Welcome from "./screen/Welcome"
+import Signup from "./screen/Signup"
 export default function App() {
-  return <Welcome></Welcome>
+  return <Signup></Signup>
 }
 const styles = StyleSheet.create({
   container: {
