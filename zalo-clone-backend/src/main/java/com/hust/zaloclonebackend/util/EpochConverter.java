1596777e3d4cb19787d6cd2d8f5c611fde5efdfe
nduc4nh <61882708+nduc4nh@users.noreply.github.com>
2021-10-08 17:26:57 +0700

    Update EpochConverter.java

--- zalo-clone-backend/src/main/java/com/hust/zaloclonebackend/util/EpochConverter.java
+++ zalo-clone-backend/src/main/java/com/hust/zaloclonebackend/util/EpochConverter.java
@@ -2,7 +2,9 @@ package com.hust.zaloclonebackend.util;
 
 import java.text.ParseException;
 
+//convert to time epoch to be stored in DB for datetime consideration
 public class EpochConverter {
+    
     public static String getCurrentEpoch() throws ParseException
     {
         Long epoch = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").parse("01/01/1970 01:00:00").getTime() / 1000;
