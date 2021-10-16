d488bbf4b184bf5a0ee30b544b638965612303e2
dinh <dinhhung.0115@gmail.com>
2021-10-16 22:27:04 +0700

    use smart git

--- frontend/constants/ErrorMessage.js
+++ frontend/constants/ErrorMessage.js
@@ -2,5 +2,5 @@ export const ERROR_MESSAGE = {
 
     INVALID_RIGHT_COMPONENT_ELEMENT: 'Invalid right component element',
     IS_MISSING_PROPS: 'is missing props. Please pass props to this component.',
-
+	SMART_GIT: 'smart git',
 }
