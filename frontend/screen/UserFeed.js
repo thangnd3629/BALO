8231ee068375a064fc8cc8a46af5615983752f3a
nduc4nh <nducanh74@gmail.com>
2021-10-29 15:15:37 +0700

    upload profile page

--- frontend/screen/UserFeed.js
+++ frontend/screen/UserFeed.js
@@ -1,15 +1,55 @@
 import React, { useEffect } from "react"
-import { StyleSheet, Text, View, Image } from "react-native"
-export default function UserFeed() {
+import { StyleSheet, Text, View, Image, FlatList } from "react-native"
+import Feed from "../components/Feed"
+import PostToolsGroup from "../components/PostToolsGroup"
+import HorizontalOptions from "../components/HorizontalOptions"
+import ProfileGroup from "../components/ProfileGroup"
+
+export default function UserFeed({ navigation, feeds }) {
+  const sections = {data: [{
+    content: "Image",
+    colors:["#D3959B","#BFE6BA"]
+  }, {
+    content: "Video",
+    colors:["#3E5151","#DECBA4"]
+  }, {
+    content: "Memories",
+    colors:["#FC466B","#3F5EFB"]
+  }]}
+
   return (
     <View style={styles.container}>
       <View style={styles.background}>
-        <Image
-          style={styles.backgroundImg}
-          source={require("../assets/story2.jpg")}
-        ></Image>
+        <ProfileGroup />
       </View>
-      <View style={styles.feed_container}></View>
+
+      <View style={styles.feed_container}>
+        <View style={styles.options}>
+          <HorizontalOptions
+            sections={sections}
+          />
+        </View>
+
+        <PostToolsGroup />
+        <FlatList
+          data={feeds}
+          renderItem={({ item }) => {
+            return (
+              <Feed
+                navigation={navigation}
+                author={item.author}
+                key={item.id + "1"}
+                can_edit={item.can_edit}
+                like={item.like}
+                comment={item.comment}
+                image={item.image}
+                id={item.id}
+                created={item.created}
+              />
+            )
+          }}
+        />
+      </View>
     </View>
   )
 }
@@ -21,11 +61,53 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
   },
+  options: {
+    flex: 1,
+    marginLeft: 10,
+    height: 100,
+  },
   backgroundImg: {
-    height: "100%",
+    height: 180,
     width: "100%",
   },
   feed_container: {
     flex: 1,
+    marginTop: "10%"
   },
+  avatarContainer: {
+    backgroundColor: "white",
+    height: 100,
+    width: 100,
+    borderRadius: 200,
+    position: 'absolute',
+    alignSelf: "center",
+    marginTop: 100,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  blueCircle: {
+    height: "95%",
+    width: "95%",
+    borderRadius: 200,
+    borderWidth: 2,
+    borderColor: 'blue',
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  avatar: {
+    height: "95%",
+    width: "95%",
+    borderRadius: 200
+  },
+  name: {
+    alignSelf: "center",
+    marginTop: 20,
+    fontWeight: "bold",
+    fontSize: 30
+  },
+  shortBio: {
+    alignSelf: "center",
+    fontSize: 16,
+    color: "#BBBFB6"
+  }
 })
