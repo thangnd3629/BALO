ceb5800d87116c9f67be63df42a022cf67a5a8c4
Nguyen Duc Thang <hsgsdominate@gmail.com>
2021-11-24 14:55:51 +0700

    Add directory screen skeleton

--- frontend/screen/Directory.js
+++ frontend/screen/Directory.js
@@ -1,11 +1,30 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native"
+import { TabView, SceneMap } from "react-native-tab-view"
 
+const MsgRoute = () => <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+
+const FriendRoute = () => <View style={{ flex: 1 }}></View>
+const renderScene = SceneMap({
+  first: FriendRoute,
+  second: MsgRoute,
+})
 export default function Directory() {
+  const layout = useWindowDimensions()
+
+  const [index, setIndex] = React.useState(0)
+  const [routes] = React.useState([
+    { key: "first", title: "Bạn bè" },
+    { key: "second", title: "Tin nhắn" },
+  ])
+
   return (
-    <View>
-      <Text>Directory</Text>
-    </View>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
   )
 }
 
